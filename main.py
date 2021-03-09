a5b4ab760c6919891bc2b7de735fef1dcfe92686
Kirill Gusev <kirillgusev@MacBook-Air-Kirill.local>
2021-03-10 00:19:53 +0300

    home

---
+++ main.py
@@ -0,0 +1,16 @@
+import datetime
+import time
+import salary
+import people
+
+DateToday = datetime.datetime.today().strftime("%d-%m-%Y")
+TimeNow = time.strftime("%H.%M.%S")
+
+print("Local Date: ", TimeNow + " " + DateToday, "\n")
+
+
+print("выполнение if __name__ == '__main__'", "\n")
+if __name__ == '__main__':
+    salary.calculate_salary()
+    people.get_employees()
+print("Код завершен")
